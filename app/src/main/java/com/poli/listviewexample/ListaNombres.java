594abaa859fe850d5a26907626cd96bf8322e68c
aflorezgut123 <andersonflorez9402@gmail.com>
2021-10-28 23:58:45 -0500

    adicion onclick

--- app/src/main/java/com/poli/listviewexample/ListaNombres.java
+++ app/src/main/java/com/poli/listviewexample/ListaNombres.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,12 +15,14 @@ public class ListaNombres extends AppCompatActivity {
 
     private ListView listaNombres;
     private ArrayList<String> nombres;
+    private EditText txtIngresarNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_nombres);
         listaNombres = findViewById(R.id.lvListNames);
+        txtIngresarNombre = findViewById(R.id.etIngresarNombre);
 
         nombres = new ArrayList<String>();
 
@@ -26,12 +30,19 @@ public class ListaNombres extends AppCompatActivity {
         nombres.add("Ivan");
         nombres.add("Alberto");
 
-        ArrayAdapter<String> listNombreAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombres);
-
-        listaNombres.setAdapter(listNombreAdapter);
+        cargarLista();
     }
 
-    public void agregarNombre(View view){
-        //String nombre = etIngresarNombre
+    public void agregarNombreBTN(View view){
+        String nombre = txtIngresarNombre.getText().toString();
+        Toast toast = Toast.makeText(getApplicationContext(), "Ingresó"+nombre, Toast.LENGTH_LONG);
+        toast.show();
+        nombres.add(nombre);
+        cargarLista();
+    }
+
+    public void cargarLista(){
+        ArrayAdapter<String> listNombreAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombres);
+        listaNombres.setAdapter(listNombreAdapter);
     }
 }
